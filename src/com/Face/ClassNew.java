67a5789d93160198a6090cc3ccadea2f6007739b
lakshmiqa1 <billaluckyrock@gmail.com>
2024-06-04 14:38:30 +0530

    my first message

---
+++ src/com/Face/ClassNew.java
@@ -0,0 +1,17 @@
+package com.Face;
+
+public class ClassNew {
+	private void one() {
+		System.out.println("1");
+
+	}
+	private void two() {
+		System.out.println("2");
+
+	}
+	private void three() {
+		System.out.println("3");
+
+	}
+
+}
